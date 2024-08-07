6329f4a62d67843872c253cb971e81f647f3e556
vonerdenRyguy <rchrisvon@gmail.com>
2024-08-07 14:15:27 -0400

    initial commit

--- lib/components/valid_word_check.dart
+++ lib/components/valid_word_check.dart
@@ -64,7 +64,6 @@ Future<List<String>> checkColumn(List<String?> letterPositions, int columnIndex,
     validWords.add(currentWord);
   }
 
-
   if (notValid.isEmpty) {
     print("Column Valid: $validWords");
     return validWords;
